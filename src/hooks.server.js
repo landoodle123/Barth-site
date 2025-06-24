d8ce65f0a31f4ef7f4a07629c95b26d03c345100
Landon <landonkazen@gmail.com>
2025-06-24 11:21:26 -0400

    made minor fixes

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,9 +4,13 @@ const prisma = new PrismaClient();
 export async function handle({ event, resolve }) {
   const session = event.cookies.get('session');
   if (session) {
-    const user = await prisma.user.findUnique({ where: { id: session } });
-    if (user) {
-      event.locals.user = { id: user.id, email: user.email };
+    try {
+      const user = await prisma.user.findUnique({ where: { id: session } });
+      if (user) {
+        event.locals.user = { id: user.id, email: user.email };
+      }
+    } catch (e) {
+      // Optionally log error
     }
   }
   return resolve(event);
