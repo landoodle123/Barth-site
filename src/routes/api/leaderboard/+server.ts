c62c1ebdc8ef87890d465569e353e9fd5947d07b
Landon <landonkazen@gmail.com>
2026-01-24 14:56:25 -0500

    tiny fix

--- src/routes/api/leaderboard/+server.ts
+++ src/routes/api/leaderboard/+server.ts
@@ -1,6 +1,6 @@
 import { prisma } from '$lib/server/prisma';
 import { json } from '@sveltejs/kit';
-import { serializeBigInts } from '../game-state/+server';
+import { _serializeBigInts } from '../game-state/+server';
 
 export const GET = async () => {
   try {
